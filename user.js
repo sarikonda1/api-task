226f1f42c500edd34865756eb344bb7b5da8b21d
Aravind kumar sarikonda <aravind.sarikonda@launchship.com>
2020-06-16 16:28:36 +0530

    update

--- user.js
+++ user.js
@@ -3,7 +3,7 @@ module.exports = mongoose.model('Users', new mongoose.Schema({
     firstName: { type: String, required: true, index: { unique: true }},
     lastName: { type: String, required: true },
     company: { type: String },
-    email: { type: String, required: true},
+    email: { type: String},
     primaryMobileNumber: { type: String},
     alterNateMobileNumber: { type: String},
 }, { timestamps: true }));
